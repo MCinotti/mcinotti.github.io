792c1bb59121fd3fc083cc8aa3a223ea9da0db96
mcinotti <martincinotti@gmail.com>
2023-08-15 21:41:13 -0400

    astro.config and CNAME

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://mcinotti.github.io',
+	base: '/mcinotti.github.io',
 	integrations: [mdx(), sitemap()],
 });
